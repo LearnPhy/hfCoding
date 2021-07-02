9d645d441b6959e8e69048451334d93134c88850
luthfi <luthfim.ilham@gmail.com>
2021-07-02 07:16:52 +0700

    uses random.choice methods

--- ch3/rockPaperScissors.py
+++ ch3/rockPaperScissors.py
@@ -1,16 +1,19 @@
-from random import randint
+from random import randint, choice
 
 #print(f'random value from 0 to 2 is : {randint(0, 2)}')
-random_choice = randint(0, 2)
-
-if random_choice == 0:
-    computer_choice = 'rock'
-elif random_choice == 1:
-    computer_choice = 'paper'
-else:
-    computer_choice = 'scissors'
+# random_choice = randint(0, 2)
+#
+# if random_choice == 0:
+#     computer_choice = 'rock'
+# elif random_choice == 1:
+#     computer_choice = 'paper'
+# else:
+#     computer_choice = 'scissors'
 
 #print('Computer choice is ',computer_choice)
 
+choices = ['rock', 'paper', 'scissors']
+random_choice = choice(choices)
+
 user_choice = input('rock, paper, scissors? ').lower()
-print('You choose', user_choice, 'and the computer choose', computer_choice)
+print('You choose', user_choice, 'and the computer choose', random_choice)
